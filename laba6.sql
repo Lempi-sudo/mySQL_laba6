fffe28a71171b2187681baf1fc67104c70da43a8
Andrei <andreii.lapschow1999@mail.ru>
2020-11-27 12:52:35 +0400

    6.1

--- laba6.sql
+++ laba6.sql
@@ -1,6 +1,32 @@
 #1. Написать процедуру, которая добавляет связанные данные в несколько
 #таблиц. В том случае, если вставка данных в одну из таблиц по какой-либо
 #причине невозможна, выполняется откат внесенных процедурой изменений.
+use sakila;
+
+drop procedure if exists add_city_country ;
+
+delimiter $$
+create procedure add_city_country(in arg_country_id smallint , in arg_country varchar(50) , in arg_city_id smallint , in agr_city varchar(50) , in agr_country_id smallint )
+begin
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+		ROLLBACK;
+		signal sqlstate '45000' set message_text = 'foriend key error';
+	END;
+	START TRANSACTION;
+    if  arg_country_id<0 or length(arg_country)=0 then
+		rollback;
+	else
+		INSERT INTO country(country_id,country,last_update) VALUE (arg_country_id, arg_country , current_timestamp());
+	end if;
+      if  arg_city_id<0 or length(agr_city)=0 or agr_country_id<0 then
+		rollback;
+	else
+		INSERT INTO city(city_id,city,country_id,last_update) VALUE (arg_city_id,agr_city,agr_country_id,current_timestamp());	
+	end if;
+	COMMIT;
+end$$
+delimiter ;
 
 #2. Написать процедуру, которая добавляет в таблицу несколько строк данных.
 #При каждом добавлении строки выполняется проверка выполнимости
